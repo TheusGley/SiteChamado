0718cee00909e423016d05633c0fe1457ad1bc56
TheusGley <81973986+TheusGley@users.noreply.github.com>
2022-04-26 18:02:42 -0400

    Add files via upload

---
+++ Insert.php
@@ -0,0 +1,28 @@
+
+
+ 
+
+     <?php
+    $nome   = $_GET=["nome"];
+    $nucleo = $_GET=["nucleo"];
+    $setor  = $_GET=["setor"];
+    $tel    = $_GET=["tel"];
+    $desc   = $_GET=["desc"];
+    $email  = $_GET=["email"];
+    $info   = $_GET=["info"];
+
+
+   // $query = " insert into chamado (nome, nucleo, setor, tel, desc, email) values ('{$nome}', '{$nucleo}','{$setor}', {$tel},'{$desc}','{$email}','{$info}')";
+    
+    //$mysql = new mysqli('127.0.0.1', 'root', '', 'bd_chamado');
+    
+   // mysqli_query($mysql, $query);
+    //mysqli_close($conexao);
+
+  $nome;
+
+  //<p class="alert-success">
+  //      Chamado:=  $nome; ,  nucleo: = $nucleo;  - adicionado com sucesso!
+//</p>
+
+    ?>
