8aee2f4f609947c0607a5b05f84abfcd40ff5865
TheusGley <81973986+TheusGley@users.noreply.github.com>
2022-04-27 18:00:31 -0400

    Update finalizado.php

--- finalizado.php
+++ finalizado.php
@@ -1,26 +1,30 @@
+<!DOCTYPE html>
 <head>
-    <meta charset="UTF-8" />
-    <title>Chamados CTI</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="style.css" />
-  </head>
-  <body>
-    <div class="container" >
-         <div id="finalizado">
-  
-          <h1 id="enviado"> Chamado enviado!!! </h1>
+
+
+</head>
+<body>
+
+<div id="finalizado">
+         
+        
+      
+          <h1 id="enviado"> Chamado enviado! </h1>
           <p> 
-              <label id="label1">Seu chamado foi enviado com sucesso, Estaremos atendendo com prioridade!!! </label>
+              <label id="label1">Seu chamado foi enviado com sucesso! Estaremos atendendo com prioridade. </label>
           </p>
           <p id="link">  
             
-            <a href="/site.chamados/chamado.php "> Voltar para chamados</a>
+            <a href="chamado.php"> Voltar para chamados</a>
           </p>
-  
-  
-        </div>    
-      
-    </div>
- 
-  </body>  
-  </html>
+
+
+</body>
+
+
+
+
+</html>
