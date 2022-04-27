265177066c5b2f924c530a8a4523f8238fd6f8f8
TheusGley <81973986+TheusGley@users.noreply.github.com>
2022-04-27 18:00:55 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+
+    <title>Chamados CTI</title>
+</head>
+<body>
+<div  align= "center"  >
+	<div>
+		<a href="chamado.php" >
+			<input id="botao" type="submit" name="chamados" value="Abrir Chamado">
+		</a>
+	</div>
+
+	<div>
+		<a href="login.php" >
+			<input  id="botao" type="submit" name="chamados" value="Login">
+		</a>
+	</div>
+</div>
+</body>
+</html>
