33d7072a9d8ad8d2ee1a4b6925c236075d9c8b9f
TheusGley <81973986+TheusGley@users.noreply.github.com>
2022-04-27 18:01:38 -0400

    Update Insert.php

--- Insert.php
+++ Insert.php
@@ -1,74 +1,33 @@
-
-
- 
-
-     <?php
-    $nome   = $_GET=["nome"];
-    $nucleo = $_GET=["nucleo"];
-    $setor  = $_GET=["setor"];
-    $tel    = $_GET=["tel"];
-    $desc   = $_GET=["desc"];
-<html> 
-  <head> 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <link rel="stylesheet" type="text/css" href="style.css" />
-  </head>
-  <body>
-  <div id="finalizado">    
-              
-              <h1 id="enviado"> Chamado enviado!!! </h1>
-              <p> 
-                  <label id="label1">Seu chamado foi enviado com sucesso, Estaremos atendendo com prioridade!!! </label>
-              </p>
-              <p id="link">  
-                
-                <a href="#parachamado"> Voltar para chamados</a>
-              </p>
-      
-              
-        
-    
-          
-</div>
-  </body>
-</html>
 <?php
-    $nome   = $_POST = ["nome"][0];
-    $nucleo = $_POST=["nucleo"];
-    $setor  = $_POST=["setor"];
-    $tel    = $_POST=["tel"];
-    $desc   = $_POST=["desc"];
-    $email  = $_POST=["email"];
-    $info   = $_POST=["info"];
+   /**
+    $nome   = $_GET=["nome"][0];
+    $nucleo = $_GET=["nucleo"][0];
+    $setor  = $_GET=["setor"][0];
+    $tel    = $_GET=["tel"][0];
+    $desc   = $_GET=["desc"][0];
+    $email  = $_GET=["email"][0];
+
+**/
+
+    $mysql = new mysqli('127.0.0.1', 'root', 'root', 'bd_site');
+
+    if ($mysql) {
+        echo("Conectado");
+    }else{
+        echo("Desconectado");
+    }
+
+ /**   $query = insert into chamados (nome, nucleo, setor, tel, desc, email) values ('$nome', '$nucleo', '$setor', '$tel', '$desc', '$email');
+
 
     
+    mysqli_query($mysql, $query);
+    mysqli_close($conexao);
 
 
-   // $query = " insert into chamado (nome) values '{$nome}')";
-   // if ($query){
-   //   echo " ";
-   // }
-  
-    
-  
-  
-  echo   " Chamado: " .$nome. " adicionado com sucesso!";
 
-    $email  = $_GET=["email"];
-    $info   = $_GET=["info"];
-
-
-   // $query = " insert into chamado (nome, nucleo, setor, tel, desc, email) values ('{$nome}', '{$nucleo}','{$setor}', {$tel},'{$desc}','{$email}','{$info}')";
-    
-    //$mysql = new mysqli('127.0.0.1', 'root', '', 'bd_chamado');
-    
-   // mysqli_query($mysql, $query);
-    //mysqli_close($conexao);
-
-  $nome;
-
-  //<p class="alert-success">
-  //      Chamado:=  $nome; ,  nucleo: = $nucleo;  - adicionado com sucesso!
-//</p>
-
-    ?>
+  <p class="alert-success">
+    Chamado:  $nome; ,  nucleo: $nucleo; setor: $setor; tel: $tel; desc: $desc; email: $email;    - adicionado com sucesso!
+  </p>
+ **/
+ ?>
