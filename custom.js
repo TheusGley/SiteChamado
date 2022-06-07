351038ae21bdf96255ee78968c6188a936e6c744
TheusGley <81973986+TheusGley@users.noreply.github.com>
2022-06-07 11:22:00 -0300

    Delete custom.js

--- custom.js
+++
@@ -1,9 +0,0 @@
-const tbody = document.querySelector("tbody");
-
-const listarchamados = async () => {
-    const dados = await fetch("./listarchamados.php");
-    const resposta = await dados.text();
-    tbody.innerHTML = resposta;
-}
-
-listarchamados();
